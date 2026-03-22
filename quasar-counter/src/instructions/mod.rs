381e1c5ddad45cc5b890855e277856841b398aeb
4rjunc <arjunmundamani@gmail.com>
2026-03-22 14:51:44 +0530

    move initialize() to instructiosn

--- quasar-counter/src/instructions/mod.rs
+++ quasar-counter/src/instructions/mod.rs
@@ -1,5 +1,7 @@
 pub mod decrement;
 pub mod increment;
+pub mod initialize;
 
 pub use decrement::*;
 pub use increment::*;
+pub use initialize::*;
--- quasar-counter/src/lib.rs
+++ quasar-counter/src/lib.rs
@@ -8,22 +8,6 @@ pub use states::*;
 
 declare_id!("9QWoDrTfMpWpgeh8qP9FqY6NdyqordB6qbfndEw6HjxY");
 
-#[derive(Accounts)]
-pub struct Initialize<'info> {
-    pub payer: &'info mut Signer,
-    #[account(init, seeds = [b"counter"], bump)]
-    pub counter: &'info mut Account<Counter>,
-    pub system_program: &'info Program<System>,
-}
-
-impl<'info> Initialize<'info> {
-    #[inline(always)]
-    pub fn initialize(&mut self, bumps: &InitializeBumps) -> Result<(), ProgramError> {
-        self.counter.set_inner(0, bumps.counter);
-        Ok(())
-    }
-}
-
 #[program]
 mod quasar_counter {
     use super::*;
