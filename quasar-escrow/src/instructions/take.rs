92746058497b1964d2b388402a59095e67ba045c
4rjunc <arjunmundamani@gmail.com>
2026-03-22 16:20:12 +0530

    feat: add take ix

---
+++ quasar-escrow/src/instructions/take.rs
@@ -0,0 +1,61 @@
+use {
+    crate::state::Escrow,
+    quasar_lang::prelude::*,
+    quasar_spl::{Mint, Token, TokenClose, TokenCpi},
+};
+
+#[derive(Accounts)]
+pub struct Take<'info> {
+    pub taker: &'info mut Signer,
+    #[account(
+        has_one = maker,
+        has_one = maker_ta_b,
+        constraint = escrow.receive > 0,
+        close = taker,
+        seeds = [b"escrow", maker],
+        bump = escrow.bump
+    )]
+    pub escrow: &'info mut Account<Escrow>,
+    pub maker: &'info mut UncheckedAccount,
+    pub mint_a: &'info Account<Mint>,
+    pub mint_b: &'info Account<Mint>,
+    #[account(init_if_needed, payer = taker, token::mint = mint_a, token::authority = taker)]
+    pub taker_ta_a: &'info mut Account<Token>,
+    pub taker_ta_b: &'info mut Account<Token>,
+    #[account(init_if_needed, payer = taker, token::mint = mint_b, token::authority = maker)]
+    pub maker_ta_b: &'info mut Account<Token>,
+    pub vault_ta_a: &'info mut Account<Token>,
+    pub rent: &'info Sysvar<Rent>,
+    pub token_program: &'info Program<Token>,
+    pub system_program: &'info Program<System>,
+}
+
+impl<'info> Take<'info> {
+    pub fn transfer_tokens(&mut self) -> Result<(), ProgramError> {
+        self.token_program
+            .transfer(
+                self.taker_ta_b,
+                self.maker_ta_b,
+                self.taker,
+                self.escrow.receive,
+            )
+            .invoke()
+    }
+
+    pub fn withdraw_tokens_and_close(&mut self, bumps: &TakeBumps) -> Result<(), ProgramError> {
+        let seeds = bumps.escrow_seeds();
+
+        self.token_program
+            .transfer(
+                self.vault_ta_a,
+                self.taker_ta_a,
+                self.escrow,
+                self.vault_ta_a.amount(),
+            )
+            .invoke_signed(&seeds)?;
+
+        self.vault_ta_a
+            .close(self.token_program, self.taker, self.escrow)
+            .invoke_signed(&seeds)
+    }
+}
--- quasar-escrow/src/lib.rs
+++ quasar-escrow/src/lib.rs
@@ -17,6 +17,18 @@ mod quasar_escrow {
         ctx.accounts.make_escrow(receive, &ctx.bumps)?;
         ctx.accounts.deposit_tokens(deposit)
     }
+
+    #[instruction(discriminator = 1)]
+    pub fn take(ctx: Ctx<Take>) -> Result<(), ProgramError> {
+        ctx.accounts.transfer_tokens()?;
+        ctx.accounts.withdraw_tokens_and_close(&ctx.bumps)
+    }
+
+    //#[instruction(discriminator = 2)]
+    //pub fn refund(ctx: Ctx<Refund>) -> Result<(), ProgramError> {
+    //    ctx.accounts.withdraw_tokens_and_close(&ctx.bumps)?;
+    //    ctx.accounts.emit_event()
+    //}
 }
 
 #[cfg(test)]
